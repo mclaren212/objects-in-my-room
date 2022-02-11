bb9a0660f363cbb03d1759ff86b2ef37e69b2e2e
Ashour Badine <mclaren212@gmail.com>
2022-02-11 00:14:54 -0600

    Initial commit

---
+++ src/objects_in_my_room/handler.clj
@@ -0,0 +1,11 @@
+(ns objects-in-my-room.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
