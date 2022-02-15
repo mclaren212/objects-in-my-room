e6d035eadd9f8c96230366af757755d372f7b906
Ashour Badine <mclaren212@gmail.com>
2022-02-14 22:07:07 -0600

    add two new objects

--- src/objects_in_my_room/objects.clj
+++ src/objects_in_my_room/objects.clj
@@ -2,7 +2,21 @@
 
 (def eli 
   {:name "eli"
-   :info "A stuffed dolphin Oriana gave me"
+   :info "A stuffed dolphin"
    :location "On top of my left speaker"
    :last-used "Don't remember"
    :source "Oriana"})
+
+(def butters
+  {:name "butters"
+   :info "Funko Pop of Butters"
+   :location "On top of my left speaker"
+   :last-used "Don't remember"
+   :source "Avak"})
+
+(def tally-counter
+  {:name "tally-counter"
+   :info "a tally counter"
+   :location "Under my monitor"
+   :last-used "Don't remember"
+   :source "Myself"})
